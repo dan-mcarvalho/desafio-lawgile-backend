2a61cfb3ebee19c27093b25d9664bd29a673a4ee
Daniel Carvalho <78363490+dan-mcarvalho@users.noreply.github.com>
2021-07-31 00:53:45 -0300

    change serverless

--- handler.js
+++ handler.js
@@ -1,4 +1,36 @@
-const handler = require('express/handler');
-const server = require('./server')
+// const schema = require('./schema');
+// const resolvers = require('./resolvers');
 
-exports.handler = handler(server);
+
+module.exports.startPostgraphile = async event => {
+  const express = require("serverless-express");
+  const { postgraphile } = require("postgraphile");
+
+  const app = express();
+
+  app.use(
+    postgraphile(
+      "postgres://Daniel:queijo123@database-lawgile.cad5ofa4qary.us-east-2.rds.amazonaws.com/postgres",
+      "postit_database",
+      { 
+        watchPg: true,
+        graphiql: true,
+        enhanceGraphiql: true,
+      }
+    )
+  );
+
+  const response = {
+      statusCode: 200,
+      headers: {
+          "Access-Control-Allow-Origin":"*",
+          "Access-Control-Allow-Credentials":"true",
+          "Access-Control-Allow-Methods":"GET,HEAD,OPTIONS,POST,PUT",
+          "Access-Control-Allow-Headers":"Access-Control-Allow-Headers:Origin,Accept:X-Requested-With:Content-Type:Access-Control-Request-Method:Access-Control-Request-Headers:Access-Control-Allow-Methods:Access-Control-Allow-Origin"
+      },
+      body: JSON.stringify('POST Method succesfully sent!'),
+  };
+
+    return response;
+}
+
--- server.js
+++
@@ -1,9 +0,0 @@
-const server = require('./app');
-
-const { PORT = 3000 } = process;
-
-server.listen(PORT, () => {
-  console.log(`Postgraphile started on port ${PORT}!`);
-});
-
-module.exports = server;
