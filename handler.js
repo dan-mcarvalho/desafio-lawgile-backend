3839465cf491d33238d90177e6f1db005c20cef5
Daniel Carvalho <78363490+dan-mcarvalho@users.noreply.github.com>
2021-07-31 19:19:36 -0300

    final commit

--- handler.js
+++ handler.js
@@ -1,36 +1,58 @@
-// const schema = require('./schema');
-// const resolvers = require('./resolvers');
+const serverless = require("serverless-http");
+const express = require('serverless-express')
+const { postgraphile } = require("postgraphile");
+const { cors } = ("middy/middleware")
+
+const app = new express();
+
+app.use(
+  postgraphile(
+    "postgres://Daniel:queijo123@database-lawgile.cad5ofa4qary.us-east-2.rds.amazonaws.com/postgres",
+    "postit_database",
+    { 
+      watchPg: true,
+      graphiql: true,
+      enhanceGraphiql: true,
+      enableCors: true
+    }
+  )
+)
+
+app.use(cors())
+
+module.exports.startPostgraphile = serverless(app);
 
 
-module.exports.startPostgraphile = async event => {
-  const express = require("serverless-express");
-  const { postgraphile } = require("postgraphile");
+// const middy = require('@middy/core')
+// const { cors } = require('middy/middlewares')
+// const { postgraphile } = require("postgraphile");
 
-  const app = express();
+// const handler = (event, context, callback) => {
 
-  app.use(
-    postgraphile(
-      "postgres://Daniel:queijo123@database-lawgile.cad5ofa4qary.us-east-2.rds.amazonaws.com/postgres",
-      "postit_database",
-      { 
-        watchPg: true,
-        graphiql: true,
-        enhanceGraphiql: true,
-      }
-    )
-  );
+//   const response = {
+//     statusCode: 200,
+//     body: "POST method was succesfull"
+//   }
 
-  const response = {
-      statusCode: 200,
-      headers: {
-          "Access-Control-Allow-Origin":"*",
-          "Access-Control-Allow-Credentials":"true",
-          "Access-Control-Allow-Methods":"GET,HEAD,OPTIONS,POST,PUT",
-          "Access-Control-Allow-Headers":"Access-Control-Allow-Headers:Origin,Accept:X-Requested-With:Content-Type:Access-Control-Request-Method:Access-Control-Request-Headers:Access-Control-Allow-Methods:Access-Control-Allow-Origin"
-      },
-      body: JSON.stringify('POST Method succesfully sent!'),
-  };
+//   return callback(null, response)
+// }
 
-    return response;
-}
+// const startPostgraphile = middy(handler)
+
+// startPostgraphile.use(cors()) 
+
+// startPostgraphile.use(
+//     postgraphile(
+//       "postgres://Daniel:queijo123@database-lawgile.cad5ofa4qary.us-east-2.rds.amazonaws.com/postgres",
+//       "postit_database",
+//       { 
+//         watchPg: true,
+//         graphiql: true,
+//         enhanceGraphiql: true,
+//         enableCors: true
+//       }
+//     )
+// )
+
+// module.exports = { startPostgraphile }
 
